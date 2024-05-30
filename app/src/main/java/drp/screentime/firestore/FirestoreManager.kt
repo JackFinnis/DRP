bc0e4db222770571dd4c05ccb2092ff04b61be5d
Timothy Langer <zeevox.dev@gmail.com>
2024-05-30 13:05:28 +0100

    fix: wait for getCompetitionData to complete
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- app/src/main/java/drp/screentime/firestore/FirestoreManager.kt
+++ app/src/main/java/drp/screentime/firestore/FirestoreManager.kt
@@ -1,22 +1,21 @@
 package drp.screentime.firestore
 
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.concurrent.CountDownLatch
 
 class FirestoreManager {
     private val db = FirebaseFirestore.getInstance()
 
     // Fetch user data
     fun getUserData(userId: String, onComplete: (User?) -> Unit) {
-        db.collection("users").document(userId).get()
-            .addOnSuccessListener { document ->
+        db.collection("users").document(userId).get().addOnSuccessListener { document ->
                 if (document != null && document.exists()) {
                     val user = document.toObject(User::class.java)
                     onComplete(user)
                 } else {
                     onComplete(null)
                 }
-            }
-            .addOnFailureListener { exception ->
+            }.addOnFailureListener { exception ->
                 onComplete(null)
             }
     }
@@ -31,8 +30,7 @@ class FirestoreManager {
                 } else {
                     onComplete(null)
                 }
-            }
-            .addOnFailureListener { exception ->
+            }.addOnFailureListener { exception ->
                 onComplete(null)
             }
     }
@@ -42,16 +40,22 @@ class FirestoreManager {
         getUserData(userId) { user ->
             if (user != null) {
                 val comps = mutableListOf<Competition>()
-                for (competitionId in user.enrolledIn) {
+                val competitionIds = user.enrolledIn
+                val latch = CountDownLatch(competitionIds.size)
+
+                for (competitionId in competitionIds) {
                     getCompetitionData(competitionId) { comp ->
                         if (comp != null) {
                             comps.add(comp)
                         }
-                        if (comps.size == user.enrolledIn.size) {
-                            onComplete(comps)
-                        }
+                        latch.countDown()
                     }
                 }
+
+                Thread {
+                    latch.await()
+                    onComplete(comps)
+                }.start()
             } else {
                 onComplete(emptyList())
             }
