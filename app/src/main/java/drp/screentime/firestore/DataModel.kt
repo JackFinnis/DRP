c0f847b034b2878d8283708fd314cc8ce608ff17
Timothy Langer <zeevox.dev@gmail.com>
2024-05-30 20:26:21 +0100

    refactor: avoid storing doc ID within doc
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- app/src/main/java/drp/screentime/firestore/DataModel.kt
+++ app/src/main/java/drp/screentime/firestore/DataModel.kt
@@ -1,12 +1,16 @@
 package drp.screentime.firestore
 
+import com.google.firebase.firestore.DocumentId
+
 data class User(
+    @DocumentId
     val id: String = "",
     val name: String = "",
     val enrolledIn: List<String> = listOf()
 )
 
 data class Competition(
+    @DocumentId
     val id: String = "",
     val name: String = "",
     val leaderboard: Map<String, Int> = mapOf() // Map of userId to score
