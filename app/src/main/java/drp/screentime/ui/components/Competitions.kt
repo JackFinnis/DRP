287f86968e04862ebaa23b3ae0f84c5853f2bd5c
jamacz <jack.mccomish@icloud.com>
2024-06-04 15:16:52 +0100

    Feat(Competitions.kt, MainActivity.kt): Add larger, labelled buttons at bottom of screen

--- app/src/main/java/drp/screentime/ui/components/Competitions.kt
+++ app/src/main/java/drp/screentime/ui/components/Competitions.kt
@@ -11,14 +11,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -33,8 +42,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import drp.screentime.firestore.Competition
 import drp.screentime.firestore.FirestoreManager
 import drp.screentime.util.formatDuration
@@ -100,22 +111,47 @@ fun UserCompetitionsScreen(
             }, modifier = modifier.fillMaxSize(), state = pullRefreshState
         ) {
             Column {
-                CompetitionList(competitions, firestoreManager, userId)
-                Row {
-                    Button(
-                        onClick = {
-                            // Add a new competition
-                            firestoreManager.createCompetitionAndAddUser(userId, "Test") { }
-                            loading = true
-                            fetchCompetitions()
-                        }) {
-                        Text("Add Competition")
+                Box(modifier = Modifier.weight(1f)) {
+                    CompetitionList(competitions, firestoreManager, userId)
+                }
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Row {
+                        MainScreenButton(
+                            modifier = modifier.weight(1f),
+                            onClick = {},
+                            icon = Icons.Filled.Info,
+                            text = "Insights"
+                        )
+                        Spacer(Modifier.width(16.dp))
+                        MainScreenButton(
+                            modifier = modifier.weight(1f),
+                            onClick = {
+                                // Add a new competition
+                                firestoreManager.createCompetitionAndAddUser(userId, "Test") { }
+                                loading = true
+                                fetchCompetitions()
+                            },
+                            icon = Icons.Filled.AddCircle,
+                            text = "Add Competition"
+                        )
                     }
-                    Button(
-                        onClick = {
-                            showJoinCompetitionDialog = true
-                        }) {
-                        Text("Join Competition")
+                    Spacer(Modifier.height(16.dp))
+                    Row {
+                        MainScreenButton(
+                            modifier = modifier.weight(1f),
+                            onClick = {
+                                showJoinCompetitionDialog = true
+                            },
+                            icon = Icons.Filled.Person,
+                            text = "Join Competition"
+                        )
+                        Spacer(Modifier.width(16.dp))
+                        MainScreenButton(
+                            modifier = modifier.weight(1f),
+                            onClick = {},
+                            icon = Icons.Filled.Face,
+                            text = "Edit Profile"
+                        )
                     }
                 }
             }
@@ -126,6 +162,24 @@ fun UserCompetitionsScreen(
                 loading -> CircularProgressIndicator()
                 error != null -> Text(text = error!!)
             }
+        }
+    }
+}
+
+@Composable
+fun MainScreenButton(modifier: Modifier, onClick: () -> Unit, icon: ImageVector, text: String) {
+    Button(
+        shape = RoundedCornerShape(16.dp),
+        contentPadding = PaddingValues(12.dp),
+        modifier = modifier,
+        onClick = onClick
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(icon, contentDescription = text, modifier = Modifier.size(36.dp))
+            Spacer(Modifier.height(4.dp))
+            Text(text, fontSize = 16.sp)
         }
     }
 }
